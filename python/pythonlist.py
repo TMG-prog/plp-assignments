3a33b274ef782d18d5495e512df1c43a76c4079d
TMG-prog <tracymuguregitau@gmail.com>
2024-12-05 20:43:48 +0300

    m

---
+++ python/pythonlist.py
@@ -0,0 +1,20 @@
+list1 = []
+
+
+list1.append(10)
+list1.append(20)
+list1.append(30)
+list1.append(40)
+
+
+list1.insert(1, 15)
+
+list1.extend([50, 60, 70])
+
+
+list1.pop()
+
+list1.sort()
+
+index30 = list1.index(30)
+print("Index of 30:", index30)
